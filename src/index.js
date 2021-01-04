979d719bcb9b29ad21852b0441a7d9af2513e94f
ll <long_sitdown@163.com>
2021-01-04 15:35:45 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.sass';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './common/style/frame.sass'
 
 ReactDOM.render(
   <React.StrictMode>
